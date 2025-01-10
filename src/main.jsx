fb8bfc8ca3a10e239061a0fcd4ce074fe0b337e8
melkie <melkieyilk@gmail.com>
2025-01-10 21:45:08 +0300

    format the visibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { UserProvider } from "./contexts/UserContext.jsx";
 
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </StrictMode>
 );
