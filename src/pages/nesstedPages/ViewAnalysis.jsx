a6a3e5b3e4346ebcf356f0fbdd5b30707214816c
melkie <melkieyilk@gmail.com>
2025-01-11 09:33:40 +0300

    inside the pharmacist dashboard to look analysis

---
+++ src/pages/nesstedPages/ViewAnalysis.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+
+const ViewAnalysis = () => {
+  const { logout } = useContext(UserContext);
+  return (
+    <div className="flex min-h-screen flex-col bg-gray-100">
+      <header className="fixed top-0 w-full flex bg-blue-900 items-center justify-between p-4 shadow-md">
+        <div className="flex items-center space-x-2">
+          <h1 className="text-2xl font-bold text-white">PharmaFinder</h1>
+        </div>
+        <button
+          className="bg-gray-100 text-blue-900 px-4 py-2 rounded-md hover:bg-gray-300"
+          onClick={logout}
+        >
+          Logout
+        </button>
+      </header>
+
+      <main>
+        <h1>View Analycis</h1>
+      </main>
+
+      <footer className="bg-blue-900 text-white p-4 mt-auto text-center">
+        <p className="text-sm">
+          &copy; 2025 Pharmacy Management. All Rights Reserved.
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default ViewAnalysis;
