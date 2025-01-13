5d5d89adf4bfe5eeff999b6fcd5694cbf8eea166
melkie <melkieyilk@gmail.com>
2025-01-13 15:16:33 +0300

    remove nessted elements and make it clean

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "./contexts/UserContext";
 
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Search from "./pages/Search";
 import PageNotFound from "./pages/PageNotFound";
-import DrugDetailsPage from "./pages/DrugDetail";
 import RegistrationPage from "./pages/Registration";
 import LoginPage from "./pages/LoginPage";
-import PharmacyDetails from "./pages/PharmacyDetail";
-import AdminDashboard from "./pages/Admin";
 import PharmacyDashboard from "./pages/PharmacyDashbourd";
+
+import ManageInventory from "./pages/nesstedPages/ManageInventory";
+import Profile from "./pages/nesstedPages/MyProfile";
+import ViewAnalysis from "./pages/nesstedPages/ViewAnalysis";
+import AddDrug from "./pages/nesstedPages/AddDrug";
+import Setting from "./pages/nesstedPages/Setting";
+import ViewInventory from "./pages/nesstedPages/ViewInventory";
+import TestAdmin from "./pages/nestedAdmin/Prac";
 
 const ProtectedRoute = ({ children, role }) => {
   const { user } = useContext(UserContext);
@@ -29,14 +32,8 @@ const ProtectedRoute = ({ children, role }) => {
 };
 
 function App() {
-  const { user } = useContext(UserContext);
-
-  const isDashboardUser =
-    user && (user.role === "admin" || user.role === "pharmacist");
-
   return (
     <BrowserRouter>
-      {!isDashboardUser && <Header />}
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/home" element={<Hero />} />
@@ -47,21 +44,68 @@ function App() {
           path="/admin"
           element={
             <ProtectedRoute role="admin">
-              <AdminDashboard />
+              <TestAdmin />
             </ProtectedRoute>
           }
         />
         <Route
-          path="/pharmacy"
+          path="/pharmacy/*"
           element={
             <ProtectedRoute role="pharmacist">
               <PharmacyDashboard />
             </ProtectedRoute>
           }
         />
+        <Route
+          path="/pharmacy/inventory"
+          element={
+            <ProtectedRoute role="pharmacist">
+              <ManageInventory />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/pharmacy/analytics"
+          element={
+            <ProtectedRoute role="pharmacist">
+              <ViewAnalysis />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/pharmacy/edit-profile"
+          element={
+            <ProtectedRoute role="pharmacist">
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/pharmacy/settings"
+          element={
+            <ProtectedRoute role="pharmacist">
+              <Setting />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/pharmacy/add-drug"
+          element={
+            <ProtectedRoute role="pharmacist">
+              <AddDrug />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/pharmacy/all-inventory"
+          element={
+            <ProtectedRoute role="pharmacist">
+              <ViewInventory />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      {!isDashboardUser && <Footer />}
     </BrowserRouter>
   );
 }
